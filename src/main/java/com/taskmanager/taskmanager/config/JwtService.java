2e29e8bc02da68f89ef0c50626b9bff829a394bf
asanali_malkenov22@icloud.com <asanali_1997@mail.ru>
2026-02-02 23:26:47 +0500

    новый коммит

--- src/main/java/com/taskmanager/taskmanager/config/JwtService.java
+++ src/main/java/com/taskmanager/taskmanager/config/JwtService.java
@@ -2,12 +2,17 @@ package com.taskmanager.taskmanager.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Component
@@ -17,25 +22,37 @@ public class JwtService {
     private final JwtSecurityConfigProperties jwtSecurityConfigProperties;
 
 
-    public String extractUsername(String token){
-        return extractClaims(token, Claims::getSubject);
-    }
+  public SecretKey getSignInKey(){
+      byte[] keyByte = Decoders.BASE64.decode(jwtSecurityConfigProperties.getSecretKey());
+      return Keys.hmacShaKeyFor(keyByte);
+  }
 
-    public <T> T extractClaims(String token, Function<Claims, T> claimsResolver){
-        final Claims claims = getAllClaimsFromToken(token);
-        return claimsResolver.apply(claims);
-    }
 
-    public Claims getAllClaimsFromToken(String token){
-        return Jwts.parserBuilder()
-                .setSigningKey(getSignInKey())
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
-    }
+  public Claims getAllClaimsFromToken(String token){
+      return Jwts.parserBuilder()
+              .setSigningKey(getSignInKey())
+              .build()
+              .parseClaimsJws(token)
+              .getBody();
+  }
 
-    public SecretKey getSignInKey(){
-        byte[] keyBytes = Decoders.BASE64.decode(jwtSecurityConfigProperties.getSecretKey());
-        return Keys.hmacShaKeyFor(keyBytes);
-    }
+
+  public <T> T extractClaims(String token,Function<Claims, T> claimsTFunction){
+      final Claims claims = getAllClaimsFromToken(token);
+      return claimsTFunction.apply(claims);
+  }
+
+  public String getUsername(String token){
+      return extractClaims(token, Claims::getSubject);
+  }
+
+  public String buildToken(Map<String, Objects> claims, UserDetails userDetails, Long expiration){
+      return Jwts.builder()
+              .setClaims(claims)
+              .setSubject(userDetails.getUsername())
+              .setIssuedAt(new Date(System.currentTimeMillis()))
+              .setExpiration(new Date(System.currentTimeMillis() + expiration))
+              .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+              .compact();
+  }
 }
